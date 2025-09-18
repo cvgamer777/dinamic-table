6a8f750d25e32608bcabc1354d6660cf8cfe0096
Tarjanyi Levente <tarjanyi.levi@gmail.com>
2025-09-18 10:43:49 +0200

    3rd commit

--- vite-project/src/components/Mytable.jsx
+++ vite-project/src/components/Mytable.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import './Mytable.css'
 
 export default class Mytable extends React.Component {
+    state = {
+        style: {
+            table: {
+                border: '1px solid black',
+                borderCollapse:'collapse'
+            },
+            td: {
+                border: '1px solid black',
+                borderCollapse:'collapse'
+            }
+           
+        }
+    }
+    
     render () {
         return (
             <div>
-                
+                <table style={this.state.style.table}>
+                    <tbody>
+                        <tr>
+                            <td style={this.state.style.td}>as</td>
+                            <td style={this.state.style.td}>asd</td>
+                            <td style={this.state.style.td}>asd</td>
+                            <td style={this.state.style.td}>asd</td>
+                        </tr>
+                    </tbody>
+                    
+                </table>
             </div>
         )
     }
