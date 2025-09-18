8c1c35b635df000ecfdb0ff9479d50565d52a923
Tarjanyi Levente <tarjanyi.levi@gmail.com>
2025-09-18 10:25:57 +0200

    2nd commit

---
+++ vite-project/src/components/Mytable.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Mytable extends React.Component {
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
